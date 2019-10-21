9cad66ad37653c61133bd9abf7fa59d1e6a5de55
Jpegzilla <jpegzilla@gmail.com>
2019-10-21 13:00:29 -0400

    update app.js

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 const myTeam = "calvin, eli";
 console.log(myTeam);
-console.log("Hi, my name is Calvin and I'm 30 years old");
+
+const createGreeting = () =>
+  console.log("Hi, my name is Calvin and I'm 30 years old");
+
+createGreeting();
