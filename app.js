a11707da75dc89a842f80a0a20266d65c23b14c1
Jekel8888 <52115322+Jekel8888@users.noreply.github.com>
2019-10-21 11:09:23 -0600

    Update app.js

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const myTeam = "calvin, eli";
 console.log(myTeam);
 
 function createGreeting(name, age) {
-  return `Hi, my ${name} is Calvin and I'm ${age} years old`;
+    let yearOfBirth = (2019-age);
+    return `Hi, my ${name} is Calvin and I'm ${age} years old, I was born in ${yearOfBirth}`;
 }
-
 const createGreeting1 = createGreeting();
 console.log(createGreeting1);
