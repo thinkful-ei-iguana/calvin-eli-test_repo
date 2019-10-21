3234a98e2e679fdd955166497f448946db6d57b9
Jekel8888 <52115322+Jekel8888@users.noreply.github.com>
2019-10-21 11:15:34 -0600

    Update app.js

--- app.js
+++ app.js
@@ -2,12 +2,17 @@ const myTeam = "calvin, eli";
 console.log(myTeam);
 
 function yearOfBirth(age) {
-  return 2019 - age;
+  if (age<0) {
+    throw new Error ('Age cannot be negative');
+  }
+    return 2019 - age;
 }
 
 function createGreeting(name, age) {
   const yob = yearOfBirth(age);
   return `Hi, my ${name} is Calvin and I'm ${age} years old, I was born in ${yob}`;
 }
+try {
 const createGreeting1 = createGreeting();
+} catch (e) {console.log(e)};
 console.log(createGreeting1);
