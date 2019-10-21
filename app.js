877e45afc850c25a81ae5844f8287049b93fbf88
Jekel8888 <52115322+Jekel8888@users.noreply.github.com>
2019-10-21 12:16:12 -0600

    Update app.js

--- app.js
+++ app.js
@@ -30,3 +30,18 @@ function jediName(firstName, lastName) {
 
   return fName + lName;
 }
+
+function beyond(num) {
+  if(num === Infinity) {
+    console.log('And beyond')
+  }
+  if(isFinite(num)&&num>=0) {
+    console.log('To infinity')
+  }
+  if(isFinite(num)&&num<0) {
+    console.log('To negative infinity')
+  }
+  if(num === 0) {
+    console.log('Staying home')
+  }
+}
