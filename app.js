4a954542734cbcd15eed7801bd7cc15d4c83c62e
Jpegzilla <jpegzilla@gmail.com>
2019-10-21 13:10:49 -0400

    update app.js

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const myTeam = "calvin, eli";
 console.log(myTeam);
 
+function yearOfBirth(age) {
+  return 2019 - age;
+}
+
 function createGreeting(name, age) {
-    let yearOfBirth = (2019-age);
-    return `Hi, my ${name} is Calvin and I'm ${age} years old, I was born in ${yearOfBirth}`;
+  const yob = yearOfBirth(age);
+  return `Hi, my ${name} is Calvin and I'm ${age} years old, I was born in ${yob}`;
 }
 const createGreeting1 = createGreeting();
 console.log(createGreeting1);
