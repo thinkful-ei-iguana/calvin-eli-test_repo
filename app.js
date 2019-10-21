98327313fea33ba6a51b13b087a1f8963f17585e
Jpegzilla <jpegzilla@gmail.com>
2019-10-21 13:17:21 -0400

    update app.js

--- app.js
+++ app.js
@@ -2,17 +2,21 @@ const myTeam = "calvin, eli";
 console.log(myTeam);
 
 function yearOfBirth(age) {
-  if (age<0) {
-    throw new Error ('Age cannot be negative');
+  if (age < 0) {
+    throw new Error("Age cannot be negative");
   }
-    return 2019 - age;
+  return 2019 - age;
 }
 
 function createGreeting(name, age) {
+  if (!name || !age) throw new Error("Arguments not valid");
+
   const yob = yearOfBirth(age);
   return `Hi, my ${name} is Calvin and I'm ${age} years old, I was born in ${yob}`;
 }
 try {
-const createGreeting1 = createGreeting();
-} catch (e) {console.log(e)};
+  const createGreeting1 = createGreeting();
+} catch (e) {
+  console.log(e);
+}
 console.log(createGreeting1);
